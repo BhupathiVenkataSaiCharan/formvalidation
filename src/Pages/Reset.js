1ea26c6a48f2852636ea8a1a1416b025f6e2c795
BhupathiVenkataSaiCharan <77479689+BhupathiVenkataSaiCharan@users.noreply.github.com>
2022-01-27 11:44:33 +0530

    changes after server host

---
+++ src/Pages/Reset.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Email from '../Toggle/forOtp/Email'
+
+
+export default function Reset() {
+    return (
+        <div>
+              <Email/>
+                 
+        </div>
+        
+    )
+}

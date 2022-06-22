ab07a9f07cb7012c7e4a1eee5d13bfe56dcf1f61
BhupathiVenkataSaiCharan <77479689+BhupathiVenkataSaiCharan@users.noreply.github.com>
2022-06-22 22:39:18 +0530

    changed

--- src/1.Routing/SideMenu/pages/Overview.js
+++ src/1.Routing/SideMenu/pages/Overview.js
@@ -6,12 +6,12 @@ const Overview = () => {
     <>
         <div className='container-fluid'>
             <div className='row'>
-            <div className='col-lg-3 text-black'>
-        </div>
-        <div className='col-lg-6 text-black'>
-            <h1>Overview</h1>
-            <Read/>
-        </div>
+              <div className='col-lg-3 text-black'>
+              </div>
+              <div className='col-lg-6 text-black'>
+                  <h1>Overview</h1>
+                  <Read/>
+              </div>
             </div>
         </div>
     </>
